d0c0b5f58b16e737785c60a6d70105e00ee38517
cecldwll <thaumaturgy101@gmail.com>
2024-04-30 10:23:24 -0600

    prep 3 learning activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        
+        Console.WriteLine("You guessed it.");
     }
 }
